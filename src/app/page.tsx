b2560cc269aa654826d99de05c7f47f25f15c575
isufiankhan <sufiancoding@gmail.com>
2024-06-20 21:55:57 +0500

    fixing mini bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,37 @@
-import GalleryGrid from "@/components/GalleryGrid";
-import SearchForm from "@/components/SearchForm";
-import UploadButton from "@/components/UploadButton"
-import cloudinary from "cloudinary";
+'use client'
 
-export type SearchResults = {
-    public_id: string;
-    tags: string[];
-};
+import { CldUploadButton } from 'next-cloudinary';
+import { CldImage } from 'next-cloudinary';
+import { useState } from 'react';
 
-const page = async ({ searchParams: { search } }: {
-    searchParams: {
-        search: string;
-    }
-}) => {
-    const results = await cloudinary.v2.search
-        .expression(`resource_type:image${search ? ` AND tags=${search}` : ""}`)
-        .sort_by('created_at', 'desc')
-        .with_field("tags")
-        .max_results()
-        .execute() as { resources: SearchResults[] };
 
-    return (
-        <div>
-            <div className="flex flex-col gap-8">
-                <div className="flex justify-between">
-                    <h1 className="text-4xl font-bold">Gallery</h1>
-                    <UploadButton />
-                </div>
-
-                <SearchForm
-                initialSearch={search}
-                />
-                <GalleryGrid
-                    images={results.resources}
-                />
-            </div>
-        </div>
-    )
+type UploadResult = {
+  info: {
+    public_id: string
+  },
+  event: 'success',
 }
 
-export default page
+export default function Home() {
+  const [imageId, setImageId] = useState("")
+  return (
+    <main>
+      <CldUploadButton
+      onUpload={(result:any) => {
+        setImageId(result.info.public_id)
+      }}
+      uploadPreset="equutn9r"
+       />
+
+      {imageId && (
+        <CldImage
+        width="300"
+        height="400"
+        src={imageId}
+        sizes="100vw"
+        alt="Description of my image"
+      />
+      )}
+    </main>
+  );
+}
